be2b2529126af2d6f6a34862ce609128872f492a
Rinesh <rinesh93bharath@gmail.com>
2021-04-21 20:32:11 +0530

    Fixed - test of error object reverted

--- app/user/core/read.js
+++ app/user/core/read.js
@@ -1,18 +1,15 @@
-// const _ = require('lodash');
 import { fetch_one_from_db } from '../../../shared/mongodb.js';
 
 const logging_key = 'read a user';
 
 export async function read (req, res, next) {
-  const filter = req.params;
-  console.log(`${logging_key} - started - ${JSON.stringify(filter)}`);
+  console.log(`${logging_key} - started - ${JSON.stringify(req.params)}`);
   try {
-    const USER = await fetch_one_from_db(logging_key, process.env.TABLE_USER, 123);
+    const USER = await fetch_one_from_db(logging_key, process.env.TABLE_USER, req.params);
     console.log(`${logging_key} - User Detail - ${JSON.stringify(USER)}`);
     res.data = USER || {};
     next();
   } catch (err) {
-    console.log('err catched');
     next(err);
   }
 };
