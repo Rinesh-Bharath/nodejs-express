be01d6d42f08fc3c5ee7d80b9c8bdd50eaf4693b
Rinesh <rinesh93bharath@gmail.com>
2021-04-21 20:27:36 +0530

    Added Error handler and response handler

--- app/server.js
+++ app/server.js
@@ -1,10 +1,19 @@
 import express from 'express';
 import cors from 'cors';
 
+import { set_environment } from '../shared/env.js';
 import userRouter from './user/router.js';
 
 const app = express();
-const port = 3000;
+
+set_environment();
+
+const userResponse = function (req, res) {
+  res.status(200).json({
+    'success': true,
+    'data': res.data || 'Ok'
+  });
+};
 
 app.use(cors());
 
@@ -23,11 +32,25 @@ router.get('/ping', function (req, res) {
   });
 });
 
-router.use('/user', userRouter);
+router.use('/user', userRouter, userResponse);
+
+// error handler
+router.use(function (err, req, res, next) {
+  res.status(404).json({
+    'success': false,
+    'data': {
+      'error': [{
+        code: err.statusCode,
+        type: err.name,
+        description: err.message
+      }]
+    }
+  });
+});
 
 // Prefix for router
 app.use('/', router);
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(process.env.NODE_PORT, () => {
+  console.log(`nGRID app listening at http://localhost:${process.env.NODE_PORT}`);
 });
--- app/user/core/read.js
+++ app/user/core/read.js
@@ -1,13 +1,18 @@
 // const _ = require('lodash');
+import { fetch_one_from_db } from '../../../shared/mongodb.js';
 
-export function read (req, res, next) {
-  const response = {
-    'first_name': 'Rinesh',
-    'last_name': 'Bharath'
-  };
+const logging_key = 'read a user';
 
-  res.status(200).json({
-    'success': true,
-    'data': response
-  });
+export async function read (req, res, next) {
+  const filter = req.params;
+  console.log(`${logging_key} - started - ${JSON.stringify(filter)}`);
+  try {
+    const USER = await fetch_one_from_db(logging_key, process.env.TABLE_USER, 123);
+    console.log(`${logging_key} - User Detail - ${JSON.stringify(USER)}`);
+    res.data = USER || {};
+    next();
+  } catch (err) {
+    console.log('err catched');
+    next(err);
+  }
 };
