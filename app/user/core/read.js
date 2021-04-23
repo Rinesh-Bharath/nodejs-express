715b9da3945d89f21a19d1065e793bbee11b451a
Rinesh <rinesh93bharath@gmail.com>
2021-04-23 23:12:23 +0530

    Added JOI Validation

--- app/user/core/read.js
+++ app/user/core/read.js
@@ -1,11 +1,19 @@
 import { fetch_one_from_db } from '../../../shared/mongodb.js';
+import pkg from 'joi';
+const Joi = pkg;
 
 const logging_key = 'read a user';
 
+const schema = Joi.object({
+  display_name: Joi.string().alphanum(),
+  age: Joi.number()
+});
+
 export async function read (req, res, next) {
-  console.log(`${logging_key} - started - ${JSON.stringify(req.params)}`);
+  console.log(`${logging_key} - started - ${JSON.stringify(req.query)}`);
   try {
-    const USER = await fetch_one_from_db(logging_key, process.env.TABLE_USER, req.params);
+    const DATA = await schema.validateAsync(req.query);
+    const USER = await fetch_one_from_db(logging_key, process.env.TABLE_USER, DATA);
     console.log(`${logging_key} - User Detail - ${JSON.stringify(USER)}`);
     res.data = USER || {};
     next();
