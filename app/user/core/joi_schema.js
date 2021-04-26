0afb1a40d9aa919a06217bc4de7013f88964f882
Deepu-uni <deepa@unimity.com>
2021-04-26 15:30:56 +0530

    Added JOI schema

---
+++ app/user/core/joi_schema.js
@@ -0,0 +1,24 @@
+import pkg from 'joi';
+const Joi = pkg;
+
+export const createSchema = Joi.object({
+  display_name: Joi.string().alphanum().min(3).max(30).required(),
+  age: Joi.number(),
+  first_name: Joi.string().pattern(new RegExp('^[a-zA-Z]')).min(3).max(30).required(),
+  last_name: Joi.string().pattern(new RegExp('^[a-zA-Z]')).min(3).max(30).required(),
+  email: Joi.string().email().required()
+});
+
+export const readSchema = Joi.object({
+  display_name: Joi.string().alphanum(),
+  age: Joi.number()
+});
+
+export const updateSchema = Joi.object({
+  display_name: Joi.string().alphanum().min(3).max(30),
+  age: Joi.number(),
+  first_name: Joi.string().pattern(new RegExp('^[a-zA-Z]')).min(3).max(30),
+  last_name: Joi.string().pattern(new RegExp('^[a-zA-Z]')).min(3).max(30),
+  email: Joi.string().email(),
+  user_id: Joi.string().required()
+});
