93adbf99197bb4827c8c5ebdb4a5c89905d77670
Rinesh <rinesh93bharath@gmail.com>
2021-04-26 17:32:38 +0530

    Added User Create API

--- app/user/core/create.js
+++ app/user/core/create.js
@@ -1,14 +1,19 @@
-// const _ = require('lodash');
+import { v4 as uuid } from 'uuid';
+import { insert_into_db } from '../../../shared/mongodb.js';
+import { createSchema } from './joi_schema.js';
 
-export function create (req, res, next) {
-  let response = {};
+const logging_key = 'create a user';
 
-  if (req.params) {
-    response = req.params;
+export async function create (req, res, next) {
+  console.log(`${logging_key} - started - ${JSON.stringify(req.body)}`);
+  try {
+    req.body.user_id = uuid();
+    const DATA = await createSchema.validateAsync(req.body);
+    const USER = await insert_into_db(logging_key, process.env.TABLE_USER, DATA);
+    console.log(`${logging_key} - User Detail - ${JSON.stringify(USER)}`);
+    res.data = USER;
+    next();
+  } catch (err) {
+    next(err);
   }
-
-  res.status(200).json({
-    'success': true,
-    'data': response
-  });
 };
