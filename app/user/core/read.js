2e2535102c563378d30651b90b8cef54f10fd469
Deepu-uni <deepa@unimity.com>
2021-04-26 15:51:49 +0530

    Added validation outside read scope

--- app/user/core/read.js
+++ app/user/core/read.js
@@ -1,18 +1,12 @@
 import { fetch_one_from_db } from '../../../shared/mongodb.js';
-import pkg from 'joi';
-const Joi = pkg;
+import { readSchema } from './joi_schema.js';
 
 const logging_key = 'read a user';
-
-const schema = Joi.object({
-  display_name: Joi.string().alphanum(),
-  age: Joi.number()
-});
 
 export async function read (req, res, next) {
   console.log(`${logging_key} - started - ${JSON.stringify(req.query)}`);
   try {
-    const DATA = await schema.validateAsync(req.query);
+    const DATA = await readSchema.validateAsync(req.query);
     const USER = await fetch_one_from_db(logging_key, process.env.TABLE_USER, DATA);
     console.log(`${logging_key} - User Detail - ${JSON.stringify(USER)}`);
     res.data = USER || {};
