7c5cdd25f3e5fe840dd47ca3d6a533e58914f265
Rinesh <rinesh93bharath@gmail.com>
2021-04-28 13:39:12 +0530

    Added duplicate email validation

--- app/user/core/create.js
+++ app/user/core/create.js
@@ -1,6 +1,7 @@
 import { v4 as uuid } from 'uuid';
 import { insert_into_db } from '../../../shared/mongodb.js';
 import { createSchema } from './joi_schema.js';
+import { validate_email_exists } from './validate.js';
 
 const logging_key = 'create a user';
 
@@ -9,6 +10,7 @@ export async function create (req, res, next) {
   try {
     req.body.user_id = uuid();
     const DATA = await createSchema.validateAsync(req.body);
+    await validate_email_exists(logging_key, DATA.email);
     const USER = await insert_into_db(logging_key, process.env.TABLE_USER, DATA);
     console.log(`${logging_key} - User Detail - ${JSON.stringify(USER)}`);
     res.data = USER;
