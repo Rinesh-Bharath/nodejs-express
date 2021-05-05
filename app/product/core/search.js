0c6b3f34fe022781e624bd5fc81d0dfd673633f2
Rinesh <rinesh93bharath@gmail.com>
2021-05-05 17:59:45 +0530

    Implemented advanced search query

--- app/product/core/search.js
+++ app/product/core/search.js
@@ -6,16 +6,62 @@ const logging_key = 'search a product';
 export async function search (req, res, next) {
   console.log(`${logging_key} - started - ${JSON.stringify(req.body)}`);
   try {
-    const filter = req.body.filter || {};
-    const sort = req.query.sort || 'PRICE_DESC';
-    const skip = req.query.skip ? parseInt(req.query.skip) : 0;
-    const limit = req.query.limit ? parseInt(req.query.limit) : 100;
+    const filter = await searchSchema.validateAsync(req.body.filter);
+    const { user_id, status, price, color, name, email } = filter || {};
+    const sort = req.body.sort || 'PRICE_DESC';
+    const skip = req.body.skip ? parseInt(req.body.skip) : 0;
+    const limit = req.body.limit ? parseInt(req.body.limit) : 10;
 
-    const inputData = await searchSchema.validateAsync(filter);
+    const aggregateQuery1 = [];
 
-    const aggregateQuery = [{
+    if (user_id) {
+      const userIdMatch = {
+        '$match': {
+          'audit.created_by': user_id,
+        }
+      };
+      aggregateQuery1.push(userIdMatch);
+    }
+
+    if (color) {
+      const colorMatch = {
+        '$match': {
+          color: color,
+        }
+      };
+      aggregateQuery1.push(colorMatch);
+    }
+
+    if (name) {
+      const nameMatch = {
+        '$match': {
+          name: name,
+        }
+      };
+      aggregateQuery1.push(nameMatch);
+    }
+
+    if (price) {
+      const priceMatch = {
+        '$match': {
+          price: price,
+        }
+      };
+      aggregateQuery1.push(priceMatch);
+    }
+
+    if (status) {
+      const statusMatch = {
+        '$match': {
+          status: status,
+        }
+      };
+      aggregateQuery1.push(statusMatch);
+    }
+
+    const aggregateQuery2 = [{
       '$lookup': {
-        'from': 'user_development',
+        'from': process.env.TABLE_USER,
         'localField': 'audit.created_by',
         'foreignField': 'user_id',
         'as': 'created_by_user'
@@ -27,7 +73,7 @@ export async function search (req, res, next) {
       }
     }, {
       '$lookup': {
-        'from': 'user_development',
+        'from': process.env.TABLE_USER,
         'localField': 'audit.updated_by',
         'foreignField': 'user_id',
         'as': 'updated_by_user'
@@ -56,15 +102,34 @@ export async function search (req, res, next) {
           'email': 1
         }
       }
-    }, {
-      '$sort': {
-        'price': 1
-      }
-    }, {
-      '$skip': skip
-    }, {
-      '$limit': limit
     }];
+
+    const aggregateQuery = [...aggregateQuery1, ...aggregateQuery2];
+
+    if (email) {
+      const nameMatch = {
+        '$match': {
+          'created_by_user.email': email,
+        }
+      };
+      aggregateQuery.push(nameMatch);
+    }
+
+    // Sort stage
+    aggregateQuery.push({
+      '$sort': findSortStage(sort)
+    });
+
+    // Skip stage
+    aggregateQuery.push({
+      '$skip': skip
+    });
+
+    // Limit stage
+    aggregateQuery.push({
+      '$limit': limit
+    });
+
     const PRODUCTS = await aggregate_from_db(logging_key, process.env.TABLE_PRODUCT, aggregateQuery);
     console.log(`${logging_key} - Products Detail - ${JSON.stringify(PRODUCTS)}`);
     res.data = PRODUCTS || [];
@@ -73,3 +138,34 @@ export async function search (req, res, next) {
     next(err);
   }
 };
+
+function findSortStage (sortValue = null) {
+  try {
+    let sort = {};
+    switch (sortValue) {
+    case 'PRICE_ASC':
+      sort = {
+        price: 1,
+      };
+      break;
+    case 'PRICE_DESC':
+      sort = {
+        price: -1,
+      };
+      break;
+    case 'NAME_ASC':
+      sort = {
+        name: 1,
+      };
+      break;
+    case 'NAME_DESC':
+      sort = {
+        name: -1,
+      };
+      break;
+    }
+    return sort;
+  } catch (err) {
+    throw new Error(err.message);
+  }
+}
